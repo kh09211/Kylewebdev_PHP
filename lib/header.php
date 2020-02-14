d61bfd90b3b6ea8a4f6de901680c63d77ee49617
kyle <Kyle>
2020-02-13 20:56:11 -0500

    Modified JS links for testing in mobile

--- lib/header.php
+++ lib/header.php
@@ -20,10 +20,11 @@
 	<link rel="shortcut icon" href="../media/favicon.ico" />
 </head>
 <body>
-	
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<div class="border-bottom border-dark">
 		<div class="header_photo text-center" id="top">
-		
 			<nav class="navbar navbar-expand-lg navbar-dark">
 			  <a class="navbar-brand" href="#"><img class="logo" src="media/gear2.png" />&nbsp<b>Kyle Hopkins</b></a>
 			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,20 +33,20 @@
 			  <div class="collapse navbar-collapse flex-row-reverse mr-3" id="navbarNav">
 			    <ul class="navbar-nav">
 			      <li class="nav-item active">
-			        <a class="nav-link" href="#resume">Resume &nbsp<span class="sr-only">(current)</span></a>
+			        <a class="nav-link" href="#resume">&nbsp Resume &nbsp<span class="sr-only">(current)</span></a>
 			      </li>
 			      <li class="nav-item">
 			        <a class="nav-link" href="#about">&nbsp About &nbsp</a>
 			      </li>
 			      <li class="nav-item">
-			        <a class="nav-link" href="#contact">&nbsp Contact</a>
+			        <a class="nav-link" href="#contact">&nbsp Contact &nbsp</a>
 			      </li>
 			    </ul>
 			  </div>
 			</nav>
 			<div class="text-center header-text">
 				<h1 class="header-text1 text-light">Full-stack Developer</h1>
-				<h5 class="text-light">This is a little snippit of text that will say something really cool about me. seriously. it will.</h5>
+				<h6 class="text-light">This is a little snippit of text that will say something really cool about me. seriously. it will.</h6>
 			</div>
 			<br>
 		
