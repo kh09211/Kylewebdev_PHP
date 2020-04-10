75cc4e97e57278ec079d1b679c4862e611b89740
kyle <Kyle>
2020-04-09 19:55:07 -0500

    added Corongram project, fixed resume layout

--- lib/resume.php
+++ lib/resume.php
@@ -13,32 +13,34 @@
   </object>
 	-->
 	<div class="row justify-content-center">
-	<ul class="ml-3"><b>Back End Web Development</b><!-- note:fix -->
-		<li>PHP 7 best practices and design principles, use of Composer and Object Orientated Programming (OOP)</li>
-		<li>MySQL and SQLite database queries, migrations, security, and CRUD use with PHP, ORMs, and APIs</li>
-		<li>GIT/Git-Hub versioning software, initializing repositories, making commits, push/pull/forking projects</li>
-		<li>Laravel framework for PHP and use of Artisan, Tinker, Blade templating, and Eloquent models</li>
-		<li>Database migrations, user authentication, controllers / routers, and use of Webpack.js, Node.js and NPM</li>
-		<li>Code debugging, parsing data, filesystem manipulation within PHP, and JSON/XML format encoding</li>
-	</ul>
-	<ul><b>Front End Web Development</b>
-		<li>HTML 5 markup, visual design, applied accessibility, and mobile first website design principles</li>
-		<li>CSS 3 and Bootstrap 4 fundamentals, CSS Flexbox, CSS Grid, and mobile responsive media queries</li>
-		<li>Javascript, jQuery AJAX, VUE.js framework, and Wordpress CMS installation and theme development</li>
-	</ul>
-	<ul><b>Linux/Unix System Administration</b>
-		<li>Proficiency with the shell command line, SSH, FTP, Bash Shell Scripting, and Linux kernel compilation</li>
-		<li>Ubuntu server, Red-hat server, Arch distributions, Package management, and use of Postfix mail MTA</li>
-		<li>AWS cloud management, EC2 instances, Route 53 and hosted zone DNS name record configurations</li>
-		<li>Apache2 and NGINX web server installation and configuration of virtual servers, and related modules</li>
-		<li>Maintain security through routine updates, upgrades, patches, and SSL / TLS certificate installations</li>
-	</ul>
-	<ul><b>Other Relevant Skills/Certs</b>
-		<li>Experience with Matlab, Basic, Visual Basic, C Complers, Decompilers, IDEC PLC Controllers and HMIs</li>
-		<li>FreeCodeCamp.org (300hr) Responsive Web Design Certification (2019)</li>
-		<li>Training and experience with LEAN Six Sigma, line balancing, efficiency, and capacity</li>
-		<li>Custom wiring harnesses, relay and controller boards, Megasquirt PCM, and standalone circuitry</li>
-		<li>Fine tuning of electronic fuel injection and ignition systems for racing application</li>
-	</ul>
+		<div class="col-md text-left offset-lg-1">
+			<ul class=""><b>Back End Web Development</b><!-- note:fix -->
+				<li>PHP 7 best practices and design principles, use of Composer and Object Orientated Programming (OOP)</li>
+				<li>MySQL and SQLite database queries, migrations, security, and CRUD use with PHP, ORMs, and APIs</li>
+				<li>GIT/Git-Hub versioning software, initializing repositories, making commits, push/pull/forking projects</li>
+				<li>Laravel framework for PHP and use of Artisan, Tinker, Blade templating, and Eloquent models</li>
+				<li>Database migrations, user authentication, controllers / routers, and use of Webpack.js, Node.js and NPM</li>
+				<li>Code debugging, parsing data, form validation, filesystem manipulation within PHP, and JSON/XML</li>
+			</ul>
+			<ul><b>Front End Web Development</b>
+				<li>HTML 5 markup, visual design, applied accessibility, and mobile first website design principles</li>
+				<li>CSS 3 and Bootstrap 4 fundamentals, CSS Flexbox, CSS Grid, and mobile responsive media queries</li>
+				<li>Javascript, jQuery AJAX, VUE.js framework, and Wordpress CMS installation and theme development</li>
+			</ul>
+			<ul><b>Linux/Unix System Administration</b>
+				<li>Proficiency with the shell command line, SSH, FTP, Bash Shell Scripting, and Linux kernel compilation</li>
+				<li>Ubuntu server, Red-hat server, Arch distributions, Package management, and use of Postfix mail MTA</li>
+				<li>AWS cloud management, EC2 instances, Route 53 and hosted zone DNS name record configurations</li>
+				<li>Apache2 and NGINX web server installation and configuration of virtual servers, and related modules</li>
+				<li>Maintain security through routine updates, upgrades, patches, and SSL / TLS certificate installations</li>
+			</ul>
+			<ul><b>Other Relevant Skills/Certs</b>
+				<li>Experience with Matlab, Basic, Visual Basic, C Complers, Decompilers, IDEC PLC Controllers and HMIs</li>
+				<li>FreeCodeCamp.org (300hr) Responsive Web Design Certification (2019)</li>
+				<li>Training and experience with LEAN Six Sigma, line balancing, efficiency, and capacity</li>
+				<li>Custom wiring harnesses, relay and controller boards, Megasquirt PCM, and standalone circuitry</li>
+				<li>Fine tuning of electronic fuel injection and ignition systems for racing application</li>
+			</ul>
+		</div>
 	</div>
 </div>
