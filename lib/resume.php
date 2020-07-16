0b58e4b48928193b38765e5345481e85a42de05b
Kyle <kh09211@georgiasouthern.edu>
2020-07-16 02:50:03 -0500

    updated resume to latest

--- lib/resume.php
+++ lib/resume.php
@@ -15,28 +15,33 @@
 	<div class="row justify-content-center">
 		<div class="col-md text-left offset-lg-2 mr-2">
 			<ul class=""><b>Back End Development</b>
-				<li>PHP 7 best practices and design principles, use of Composer and Object Orientated Programming (OOP)</li>
-				<li>MySQL and SQLite database queries, migrations, security, and CRUD use with PHP, ORMs, and APIs</li>
-				<li>GIT/Git-Hub versioning software, initializing repositories, making commits, push/pull/forking projects</li>
-				<li>Laravel framework for PHP and use of Artisan, Tinker, Blade templating, and Eloquent models</li>
-				<li>Database migrations, user authentication, controllers / routers, and use of Webpack.js, Node.js and NPM</li>
-				<li>Code debugging, parsing data, form validation, filesystem manipulation within PHP, and JSON/XML</li>
+				<li>PHP 7 best practices and design principles, Object Orientated Programming, and use of Composer</li>
+				<li>Laravel framework for PHP and use of Artisan, Blade templating, Eloquent models, Tinker, database 
+			<br>migrations user authentication, authorization, routes, controllers, webpack, and caching with Reddis</li>
+				<li>MySQL and SQLite database migrations, queries, security, and CRUD use with ORMs, PDO, and API</li>
+				<li>GIT/Git-Hub versioning software, initializing repositories, cloning, forking, commits, revert versions</li>
+				<li>Code debugging, parsing data, form validation, filesystem manipulation, and JSON/XML formating</li>
+				<li>Payment integration using Stripe API, Laravel Cashier, WooCommerce, and managing Wordpress</li>
 			</ul>
 			<ul><b>Front End Development</b>
+				<li>Javascript (ES6) Object Oriented Programming and app design principles, algorithm scripting and
+			<br> data structures, regular expressions, debugging, AJAX calls, Axios, jQuery, and custom carousels</li>
+				<li>Vue.js framework for Javascript and the use of Vuex, Vue-CLI, Vue Router, NPM, and Node.js</li>
+				<li>Experience with Zeplin, Figma, and turning UI/UX designs into functional pixel perfect front ends</li>
 				<li>HTML 5 markup, visual design, applied accessibility, and mobile first website design principles</li>
-				<li>CSS 3 and Bootstrap 4 fundamentals, CSS Flexbox, CSS Grid, and mobile responsive media queries</li>
-				<li>Javascript, jQuery AJAX, VUE.js framework, and Wordpress CMS installation and theme development</li>
+				<li>CSS 3 and Bootstrap 4 proficiency, CSS Flexbox, CSS Grid, SASS, and mobile responsive media queries</li>
 			</ul>
 			<ul><b>System Administration</b>
-				<li>Proficiency with the shell command line, SSH, FTP, Bash Shell Scripting, and Linux kernel compilation</li>
-				<li>Ubuntu server, Red-hat server, Arch distributions, Package management, and use of Postfix mail MTA</li>
+				<li>Deployment  of  application code to servers, setting up databases, caching, special configurations</li>
+				<li>Proficiency with the shell command line, SSH, FTP, shell scripting, and custom kernel compliation</li>
+				<li>Ubuntu server, Red-hat server, Arch distributions, package management, Postfix mail MTA and config</li>
 				<li>AWS cloud management, EC2 instances, Route 53 and hosted zone DNS name record configurations</li>
 				<li>Apache2 and NGINX web server installation and configuration of virtual servers, and related modules</li>
 				<li>Maintain security through routine updates, upgrades, patches, and SSL / TLS certificate installations</li>
 			</ul>
 			<ul><b>Other Relevant Skills/Certs</b>
-				<li>Experience with Matlab, Basic, Visual Basic, C Complers, Decompilers, IDEC PLC Controllers and HMIs</li>
 				<li>FreeCodeCamp.org (300hr) Responsive Web Design Certification (2019)</li>
+				<li>Experience with Matlab, Basic, Visual Basic, C Complers, Decompilers, IDEC PLC Controllers and HMIs</li>
 				<li>Training and experience with LEAN Six Sigma, line balancing, efficiency, and capacity</li>
 				<li>Custom wiring harnesses, relay and controller boards, Megasquirt PCM, and standalone circuitry</li>
 				<li>Fine tuning of electronic fuel injection and ignition systems for racing application</li>
